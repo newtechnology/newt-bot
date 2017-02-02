b5cb06f10bf2f7cce37032acd5fea7a498da77ad
newtechnology <abhijeetzala81@gmail.com>
2017-02-02 23:34:54 +0530

    beautification of code

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -9,12 +9,17 @@ class Program
 
     private DiscordClient _client;
 
-    string[] greetings = { "hey", "hi", "hello", "sup" , "morgen"};
+    string[] greetings = { "hey", "hi", "hello" };
 
 
-    string QuoteCommand = ".quote";
+
+    //bot commands
+
+    string[] Commands = { ".quote", ".seen" };
+    enum ECommands { QUOTE, SEEN };
 
 
+    //gets a line at a given index in a file
     string GetLine(string fileName, int line)
     {
         using (var sr = new StreamReader(fileName))
@@ -34,6 +39,50 @@ class Program
     }
 
 
+    //commands which don't require a nickname like .help, .time, .uptime, etc...
+    bool IsCommand(string message, ECommands command)
+    {
+        if (message == Commands[(int)command])
+            return true;
+
+        return false;
+    }
+
+    //commands which contain a nickname like .quote newt, .seen newt, .burn newt, etc..
+    bool IsCommand(string message, ECommands command, out string output_Nickname)
+    {
+
+        output_Nickname = "";
+
+        char[] DelimeterChars = { ' ' };
+
+        string[] words = message.Split(DelimeterChars);
+
+        if (words[0] == Commands[(int)command])
+        {
+            if (words.Length > 2)
+            {
+                for (int i = 1; i < words.Length; ++i)
+                {
+
+                    output_Nickname += words[i];
+
+                    if (i != words.Length - 1)
+                    output_Nickname += " ";
+                }
+            }
+            else if (words.Length == 2)
+            {
+                output_Nickname = words[1];
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
     public void Start()
     {
         Console.Title = "newt-bot";
@@ -49,7 +98,7 @@ class Program
             {
 
 
-                #region LogEverythingOnConsole
+                #region Log Everything On Console
 
                 //write user's name in a different color
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -67,11 +116,24 @@ class Program
                 Console.ResetColor();
                 #endregion
 
-                #region RecordMessages
+                #region Record Messages
 
                 string FileName = e.Message.User.Name + ".txt";
 
-                if (!(e.Message.Text.Contains(QuoteCommand)))
+
+                bool RecordThisMessage = true;
+
+
+                foreach (string c in Commands)
+                {
+                    if (ICS_Contains(e.Message.Text, c))
+                    {
+                        RecordThisMessage = false;
+                    }
+                }
+
+
+                if (RecordThisMessage)
                 {
 
                     StreamWriter file = new StreamWriter(FileName, true);
@@ -86,7 +148,7 @@ class Program
                 
                 #endregion
 
-                #region IsMentioningMe
+                #region Is Mentioning Me
                 if (e.Message.IsMentioningMe())
                 {
                     await e.Channel.SendMessage(e.Message.User.Name + ", why are you mentioning me?");
@@ -103,12 +165,11 @@ class Program
                 }
                 #endregion
 
-
-
-                #region SwearAtUsers_Iff_TheySwearAtMe
+                #region Swear at users
 
                 if (ICS_Contains(e.Message.Text, "fuck") && (ICS_Contains(e.Message.Text, "newt-bot")|| ICS_Contains(e.Message.Text, "newt bot") || ICS_Contains(e.Message.Text, "newtbot")))
                 {
+
                     await e.Channel.SendMessage("You need to shut up, " + e.Message.User.Name + ".");
                 }
 
@@ -116,76 +177,57 @@ class Program
 
                 #region Quote a random quote
 
-
-                //quoting yourself
-                //===========================================================================================================//
-                if (string.Equals(e.Message.Text, QuoteCommand, System.StringComparison.CurrentCultureIgnoreCase))
+                string out_NICK;
+                if (IsCommand(e.Message.Text, ECommands.QUOTE, out out_NICK))
                 {
-                   if (File.Exists(FileName))
+                   if (File.Exists(out_NICK + ".txt"))
                     {
-                        var NumberOfQuotes = File.ReadAllLines(FileName).Length;
+                        var NumberOfQuotes = File.ReadAllLines(out_NICK + ".txt").Length;
 
                         Random r = new Random();
                         int i = r.Next(0, NumberOfQuotes);
 
-                        string Quote = GetLine(FileName, i);
-                        string channel = GetLine(FileName + "_channel.txt", i);
+                        string Quote = GetLine(out_NICK + ".txt", i);
+                        string channel = GetLine(out_NICK + ".txt" + "_channel.txt", i);
 
                         await e.Channel.SendMessage(@e.Message.User.Name + " once said in #" + channel + ": " + "\"" + Quote + "\"");
 
                     }
                    else
                     {
-                        await e.Channel.SendMessage("I don't know any quotes from you.");
+                        await e.Channel.SendMessage("I don't know any quotes from " + out_NICK + ".");
                     }
                 }
-                //===========================================================================================================//
 
 
-                //quoting someone else
-                //===========================================================================================================//
-                string QuoteCommandOneSpace = QuoteCommand + " ";
 
-                foreach (Discord.User user in e.Channel.Users)
+                #endregion
+
+                #region Quote Last Quote (When was this user last seen)
+
+                if (IsCommand(e.Message.Text, ECommands.SEEN, out out_NICK))
                 {
-                    if (user.IsBot)
-                        continue;
-
-                    string t = QuoteCommandOneSpace + user.Name;
-
-                    if (string.Equals(e.Message.Text, t, System.StringComparison.CurrentCultureIgnoreCase))
+                    if (File.Exists(out_NICK + ".txt"))
                     {
-                        if (File.Exists(user.Name + ".txt"))
-                        {
-                            var NumberOfQuotes = File.ReadAllLines(user.Name + ".txt").Length;
+                        var NumberOfQuotes = File.ReadAllLines(out_NICK + ".txt").Length;
 
-                            Random r = new Random();
-                            int i = r.Next(0, NumberOfQuotes);
+                        string Quote = GetLine(out_NICK + ".txt", NumberOfQuotes - 1);
+                        string channel = GetLine(out_NICK + ".txt" + "_channel.txt", NumberOfQuotes - 1);
 
-                            string Quote = GetLine(user.Name + ".txt", i);
-                            string channel = GetLine(user.Name + ".txt" + "_channel.txt", i);
+                        await e.Channel.SendMessage(@e.Message.User.Name + " was last seen in #" + channel + " saying" + ": " + "\"" + Quote + "\"");
 
-                            await e.Channel.SendMessage(@user.Name + " once said in #" + channel + ": " + "\"" + Quote + "\"");
-
-                        }
-                        else
-                        {
-                            await e.Channel.SendMessage("I don't know any quotes from " + user.Name + ".");
-                        }
                     }
-
+                    else
+                    {
+                        await e.Channel.SendMessage(out_NICK + " has never been seen.");
+                    }
                 }
-
-
-                //===========================================================================================================//
-
 
                 #endregion
 
 
 
-
-            }
+                }
         };
 
         _client.ExecuteAndWait(async () => 
